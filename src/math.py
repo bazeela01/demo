1e0fdeb8d5a06cac7e2b35f4ce8a5c105dcb43f6
bazeela01 <bazeela01@gmail.com>
2025-10-05 19:37:44 +0530

    test

--- src/math.py
+++ src/math.py
@@ -5,8 +5,8 @@ def add(x,y):
     return x+y 
 #sub()
 def subtract(x,y):
-    pass
+    return x-y
 def multiply(x,y):
-    pass
+    return x*y
 def divide(x,y):
     pass
--- test/test_maths.py
+++ test/test_maths.py
@@ -6,3 +6,18 @@ import src.math as t
 def test_addition(x,y):
     res = t.add(x,y)
     assert res == x+y       
+
+
+@pytest.mark.parametrize("x,y" , [(87,19),(788,16)])
+def test_substraction(x,y):
+    res = t.subtract(x,y)
+    assert res == x-y  
+
+@pytest.mark.parametrize("x,y" , [(8,2),(78,6)])
+def test_multiply(x,y):
+    res = t.multiply(x,y)
+    assert res == x*y  
+
+
+
+# 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git 
